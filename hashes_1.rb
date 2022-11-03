41ef069e69757ab290ea2d6e7e1a4171d82c3183
Dawson Timmons <DawsonTimmons@gmail.com>
2022-11-03 15:27:34 -0600

    Add answers to part 1 in hashes_1

--- hashes_1.rb
+++ hashes_1.rb
@@ -6,24 +6,34 @@ Activity:
 # file by entering the following command in your Terminal: 
 # `ruby hashes_1.rb`
 
-foods = {"apples": 23, "grapes": 507, "eggs": 48}
-p zoo
+foods = {
+  apples: 23, 
+  grapes: 507, 
+  eggs: 48
+}
 
 # Write code that prints all of the 'keys' of the foods variable 
 # you created above:
-# YOUR CODE HERE
+
+puts foods.keys
 
 # Write code that prints all of the 'values' of the foods variable 
 # you created above:
-# YOUR CODE HERE
+
+puts food.values
 
 # Write code that prints the value of the second food of the foods variable 
 # you created above:
-# YOUR CODE HERE
+ 
+puts foods.values[1]
 
 # Write code that adds a food to the foods hash. 
 # Then, print the updated hash:
-# YOUR CODE HERE
+
+foods.store(:Bread, 100)
+
+puts foods
+
 
 
 #-------------------
